4f71d1fba6f5b16a21da24fd9f1f060401b773b8
PoHaoHsu <haohsu0823@gmail.com>
2017-03-20 13:41:34 -0500

    Lab3

---
+++ sketch_mar13a/sketch_mar13a.ino
@@ -0,0 +1,25 @@
+#include <Grove_LED_Bar.h>
+#include <math.h>
+
+const int pinLight = A0;
+Grove_LED_Bar bar(7, 6, 0);
+
+void setup() {
+  // put your setup code here, to run once:
+  bar.begin();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int lightValue = analogRead(pinLight);
+  
+  if(lightValue > 0){
+    int i = log(lightValue);
+    bar.setLevel(i);
+  }
+  else{
+    bar.setLevel(0);
+  }
+
+  delay(100);
+}
